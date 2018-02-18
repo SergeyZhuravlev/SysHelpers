f41743680583bbe4a44753a4509a107e286c435e
Sergey Zhuravlev <sergey4zhuravlev@gmail.com>
2018-02-18 18:07:29 +0300

    Renamed backup branch in Windows/Subversions/Git/WaitingForPushAvailable.cmd

--- Windows/Subversions/Git/WaitingForPushAvailable.cmd
+++ Windows/Subversions/Git/WaitingForPushAvailable.cmd
@@ -1,8 +1,8 @@
 ::cd directory_with_code
 title Waiting for push available...
 echo Waiting for push available...
-git branch -D z/Backup2920
-git branch z/Backup2920 || goto backupFailed
+git branch -D z/AutoBackupBranchBeforePullRebase2920
+git branch z/AutoBackupBranchBeforePullRebase2920 || goto backupFailed
 :waitingLoop
 (git pull --rebase || goto rebaseFailed) && git push && goto success
 ping 127.0.0.1 -n 3
@@ -14,7 +14,7 @@ exit
 :rebaseFailed
 echo Rebase failed. We will try aborting "git pull --rebase". Try aborting by:
 pause
-(git rebase --abort || del .git/rebase-apply) && git reset z/Backup2920 && echo Aborting rebase probably success. && pause && exit
+(git rebase --abort || del .git/rebase-apply) && git reset z/AutoBackupBranchBeforePullRebase2920 && echo Aborting rebase probably success. && pause && exit
 echo Aborting rebase failed.
 pause
 exit
